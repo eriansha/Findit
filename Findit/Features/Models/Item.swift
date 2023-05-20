b976fea2b3f2189f279170704c653a715d996926
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-05-20 14:29:50 +0700

    feat: init Item model

---
+++ Findit/Features/Models/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Findit
+//
+//  Created by Ivan on 20/05/23.
+//
+
+import Foundation
+
+struct Item: Identifiable, Hashable {
+    let id: UUID
+    var logo: String
+    var name: String
+    var beaconUUID: UUID
+    var beaconMajor: Int
+    var beaconMinor: Int
+}

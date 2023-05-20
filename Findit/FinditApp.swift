8af1c66a54ff53bb00c67ff69292527e488aea6d
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-05-20 14:18:25 +0700

    Initial Commit

---
+++ Findit/FinditApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinditApp.swift
+//  Findit
+//
+//  Created by Ivan on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FinditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

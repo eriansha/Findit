c49360d8705f78b0b62390f331ddc426ff542a41
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-05-20 18:07:12 +0700

    feat: init InputField

---
+++ Findit/Features/Presenters/Components/InputField.swift
@@ -0,0 +1,51 @@
+//
+//  InputField.swift
+//  Findit
+//
+//  Created by Ivan on 20/05/23.
+//
+
+import SwiftUI
+
+struct InputField: View {
+    public var label = "Name"
+    @State public var value = "Bag"
+    
+    @State private var isTextFieldFocused: Bool = false
+    
+    var borderColor: Color {
+        isTextFieldFocused ? .blue : .gray
+    }
+    
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text(label)
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, -8)
+                
+                TextField("", text: $value)
+                    .onTapGesture {
+                       isTextFieldFocused = true
+                   }
+            }.padding(8)
+        }
+        .padding(4)
+        .cornerRadius(8.0)
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(borderColor, lineWidth: 2)
+        )
+        .padding(.horizontal, 8)
+    }
+}
+
+struct InputField_Previews: PreviewProvider {
+    static var previews: some View {
+        InputField(
+            label: "Name",
+            value: "Bag"
+        )
+    }
+}
